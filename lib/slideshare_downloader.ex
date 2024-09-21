8a847c8e0ae5b705b2b419aed659e38ad3c13819
Otter-K <k.kono.techs@gmail.com>
2024-09-21 22:04:07 +0900

    initial commit

---
+++ lib/slideshare_downloader.ex
@@ -0,0 +1,18 @@
+defmodule SlideshareDownloader do
+  @moduledoc """
+  Documentation for `SlideshareDownloader`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SlideshareDownloader.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/slideshare_downloader_test.exs
@@ -0,0 +1,8 @@
+defmodule SlideshareDownloaderTest do
+  use ExUnit.Case
+  doctest SlideshareDownloader
+
+  test "greets the world" do
+    assert SlideshareDownloader.hello() == :world
+  end
+end
